b7681b017114b45109c5358a8942a27a96a6fc2d
FabianGal45 <fabiangal.45@gmail.com>
2022-09-27 11:58:54 +0100

    Added console commands

--- src/main/java/eu/ovmc/waystones/commands/Ws.java
+++ src/main/java/eu/ovmc/waystones/commands/Ws.java
@@ -13,10 +13,13 @@ import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
+
+import static org.bukkit.Bukkit.getServer;
 
 public class Ws implements CommandExecutor {
     @Override
@@ -24,8 +27,6 @@ public class Ws implements CommandExecutor {
 
         if(sender instanceof Player){
             Player player = (Player) sender;
-
-
 
             if(args.length==0){
                 WaystonesPlugin plugin = WaystonesPlugin.getPlugin();
@@ -38,10 +39,10 @@ public class Ws implements CommandExecutor {
                     user.purchaseWaystone(playerMenuUtility);
                 }
                 else{
-                    player.sendMessage(Component.text("Something went wrong when purchasing a waystone", NamedTextColor.RED)); //Todo: Test this
+                    player.sendMessage(Component.text("Something went wrong when purchasing a waystone", NamedTextColor.RED)); //This might never be triggered
                 }
             }
-            else if (args[0].equals("set")) {
+            else if (args[0].equals("add")) {
                 if(player.hasPermission("waystones.admin")){
                     if(args[1].equals("public")){
                         if(args.length>3){
@@ -56,9 +57,9 @@ public class Ws implements CommandExecutor {
                                 User user = playerMenuUtility.getUser();
 
                                 try{
-                                    user.setAcquiredPublicWs(Integer.parseInt(args[3]));
+                                    user.setAcquiredPublicWs(user.getAcquiredPublicWs() + Integer.parseInt(args[3]));
                                     jdbc.updateUser(user);
-                                    player.sendMessage("Set "+ args[3] + " public waystones to " + target.getName());
+                                    player.sendMessage("Added "+ args[3] + " public waystones to " + target.getName());
                                 }
                                 catch (NumberFormatException e){
                                     player.sendMessage(args[3]+" has to be a number.");
@@ -69,9 +70,9 @@ public class Ws implements CommandExecutor {
                                 User user = jdbc.getUserFromDB(target.getUniqueId().toString());
                                 if(user != null){
                                     try{
-                                        user.setAcquiredPublicWs(Integer.parseInt(args[3]));
+                                        user.setAcquiredPublicWs(user.getAcquiredPublicWs() + Integer.parseInt(args[3]));
                                         jdbc.updateUser(user);
-                                        player.sendMessage("Set "+ args[3] + " public waystones to " + target.getName());
+                                        player.sendMessage("Added "+ args[3] + " public waystones to " + target.getName());
                                     }
                                     catch (NumberFormatException e){
                                         player.sendMessage(args[3]+" has to be a number.");
@@ -98,9 +99,9 @@ public class Ws implements CommandExecutor {
                                 User user = playerMenuUtility.getUser();
 
                                 try{
-                                    user.setAcquiredPrivateWs(Integer.parseInt(args[3]));
+                                    user.setAcquiredPrivateWs(user.getAcquiredPrivateWs() + Integer.parseInt(args[3]));
                                     jdbc.updateUser(user);
-                                    player.sendMessage("Set "+ args[3] + " private waystones to " + target.getName());
+                                    player.sendMessage("Added "+ args[3] + " private waystones to " + target.getName());
                                 }
                                 catch (NumberFormatException e){
                                     player.sendMessage(args[3]+" has to be a number.");
@@ -111,9 +112,9 @@ public class Ws implements CommandExecutor {
                                 User user = jdbc.getUserFromDB(target.getUniqueId().toString());
                                 if(user != null){
                                     try{
-                                        user.setPurchasedPrivateWs(Integer.parseInt(args[3]));
+                                        user.setAcquiredPrivateWs(user.getAcquiredPrivateWs() + Integer.parseInt(args[3]));
                                         jdbc.updateUser(user);
-                                        player.sendMessage("Set "+ args[3] + " private waystones to " + target.getName());
+                                        player.sendMessage("Added "+ args[3] + " private waystones to " + target.getName());
                                     }
                                     catch (NumberFormatException e){
                                         player.sendMessage(args[3]+" has to be a number.");
@@ -139,6 +140,180 @@ public class Ws implements CommandExecutor {
             }
 
 
+        }
+        else{
+            ConsoleCommandSender console = getServer().getConsoleSender();
+
+            if (args[0].equals("set")) {
+                if (args[1].equals("public")) {
+                    if (args.length > 3) {
+                        OfflinePlayer target = Bukkit.getOfflinePlayer(args[2]);
+                        SQLiteJDBC jdbc = WaystonesPlugin.getPlugin().getJdbc();
+
+                        if (target.isOnline()) {
+                            System.out.println("UUID Online " + target.getUniqueId());
+
+                            PlayerMenuUtility playerMenuUtility = WaystonesPlugin.getPlayerMenuUtility(target.getPlayer());
+                            User user = playerMenuUtility.getUser();
+
+                            try {
+                                user.setAcquiredPublicWs(Integer.parseInt(args[3]));
+                                jdbc.updateUser(user);
+                                console.sendMessage("Set " + args[3] + " public waystones to " + target.getName());
+                            } catch (NumberFormatException e) {
+                                console.sendMessage(args[3] + " has to be a number.");
+                            }
+                        } else {
+                            User user = jdbc.getUserFromDB(target.getUniqueId().toString());
+                            if (user != null) {
+                                try {
+                                    user.setAcquiredPublicWs(Integer.parseInt(args[3]));
+                                    jdbc.updateUser(user);
+                                    console.sendMessage("Set " + args[3] + " public waystones to " + target.getName());
+                                } catch (NumberFormatException e) {
+                                    console.sendMessage(args[3] + " has to be a number.");
+                                }
+
+
+                            } else {
+                                console.sendMessage("This user does not exist in the database.");
+                            }
+                        }
+                    }
+                } else if (args[1].equals("private")) {
+                    if (args.length > 3) {
+
+                        OfflinePlayer target = Bukkit.getOfflinePlayer(args[2]);
+                        SQLiteJDBC jdbc = WaystonesPlugin.getPlugin().getJdbc();
+
+                        if (target.isOnline()) {
+                            System.out.println("UUID Online " + target.getUniqueId());
+
+                            PlayerMenuUtility playerMenuUtility = WaystonesPlugin.getPlayerMenuUtility(target.getPlayer());
+                            User user = playerMenuUtility.getUser();
+
+                            try {
+                                user.setAcquiredPrivateWs(Integer.parseInt(args[3]));
+                                jdbc.updateUser(user);
+                                console.sendMessage("Set " + args[3] + " private waystones to " + target.getName());
+                            } catch (NumberFormatException e) {
+                                console.sendMessage(args[3] + " has to be a number.");
+                            }
+
+
+                        } else {
+                            User user = jdbc.getUserFromDB(target.getUniqueId().toString());
+                            if (user != null) {
+                                try {
+                                    user.setAcquiredPrivateWs(Integer.parseInt(args[3]));
+                                    jdbc.updateUser(user);
+                                    console.sendMessage("Set " + args[3] + " private waystones to " + target.getName());
+                                } catch (NumberFormatException e) {
+                                    console.sendMessage(args[3] + " has to be a number.");
+                                }
+
+
+                            } else {
+                                console.sendMessage("This user does not exist in the database.");
+                            }
+                        }
+                    }
+                } else {
+                    console.sendMessage(Component.text("private or public?", NamedTextColor.DARK_RED));
+                }
+            }
+            else if (args[0].equals("add")) {
+                if(args[1].equals("public")){
+                    if(args.length>3){
+
+                        OfflinePlayer target = Bukkit.getOfflinePlayer(args[2]);
+                        SQLiteJDBC jdbc = WaystonesPlugin.getPlugin().getJdbc();
+
+                        if(target.isOnline()){
+                            System.out.println("UUID Online " + target.getUniqueId());
+
+                            PlayerMenuUtility playerMenuUtility = WaystonesPlugin.getPlayerMenuUtility(target.getPlayer());
+                            User user = playerMenuUtility.getUser();
+
+                            try{
+                                user.setAcquiredPublicWs(user.getAcquiredPublicWs() + Integer.parseInt(args[3]));
+                                jdbc.updateUser(user);
+                                console.sendMessage("Added "+ args[3] + " public waystones to " + target.getName());
+                            }
+                            catch (NumberFormatException e){
+                                console.sendMessage(args[3]+" has to be a number.");
+                            }
+
+
+                        }else{
+                            User user = jdbc.getUserFromDB(target.getUniqueId().toString());
+                            if(user != null){
+                                try{
+                                    user.setAcquiredPublicWs(user.getAcquiredPublicWs() + Integer.parseInt(args[3]));
+                                    jdbc.updateUser(user);
+                                    console.sendMessage("Added "+ args[3] + " public waystones to " + target.getName());
+                                }
+                                catch (NumberFormatException e){
+                                    console.sendMessage(args[3]+" has to be a number.");
+                                }
+
+
+                            }
+                            else{
+                                console.sendMessage("This user does not exist in the database.");
+                            }
+                        }
+                    }
+                }
+                else if(args[1].equals("private")){
+                    if(args.length>3){
+
+                        OfflinePlayer target = Bukkit.getOfflinePlayer(args[2]);
+                        SQLiteJDBC jdbc = WaystonesPlugin.getPlugin().getJdbc();
+
+                        if(target.isOnline()){
+                            System.out.println("UUID Online " + target.getUniqueId());
+
+                            PlayerMenuUtility playerMenuUtility = WaystonesPlugin.getPlayerMenuUtility(target.getPlayer());
+                            User user = playerMenuUtility.getUser();
+
+                            try{
+                                user.setAcquiredPrivateWs(user.getAcquiredPrivateWs() + Integer.parseInt(args[3]));
+                                jdbc.updateUser(user);
+                                console.sendMessage("Added "+ args[3] + " private waystones to " + target.getName());
+                            }
+                            catch (NumberFormatException e){
+                                console.sendMessage(args[3]+" has to be a number.");
+                            }
+
+
+                        }else{
+                            User user = jdbc.getUserFromDB(target.getUniqueId().toString());
+                            if(user != null){
+                                try{
+                                    user.setAcquiredPrivateWs(user.getAcquiredPrivateWs() + Integer.parseInt(args[3]));
+                                    jdbc.updateUser(user);
+                                    console.sendMessage("Added "+ args[3] + " private waystones to " + target.getName());
+                                }
+                                catch (NumberFormatException e){
+                                    console.sendMessage(args[3]+" has to be a number.");
+                                }
+
+
+                            }
+                            else{
+                                console.sendMessage("This user does not exist in the database.");
+                            }
+                        }
+                    }
+                }
+                else{
+                    console.sendMessage(Component.text("private or public?", NamedTextColor.DARK_RED));
+                }
+            }
+            else{
+                console.sendMessage(Component.text("This command does not exist", NamedTextColor.DARK_RED));
+            }
         }
 
 
